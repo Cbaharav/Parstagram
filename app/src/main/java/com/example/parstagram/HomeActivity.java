c173de8e5cd02d0c76126473d78a1ed7c2ca9178
Cbaharav <carmel.baharav@gmail.com>
2019-07-08 15:57:38 -0700

    users can log out, returning to the sign in page

--- app/src/main/java/com/example/parstagram/HomeActivity.java
+++ app/src/main/java/com/example/parstagram/HomeActivity.java
@@ -1,17 +1,24 @@
 package com.example.parstagram;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.parstagram.model.Post;
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseUser;
 
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+
+    MenuItem miLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +43,21 @@ public class HomeActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // inflate the menu; this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // when logout menu item is clicked
+    public void onLogOut(MenuItem mi) {
+        ParseUser.logOut();
+        Toast.makeText(this, "Logged out!", Toast.LENGTH_LONG).show();
+        Intent i = new Intent(HomeActivity.this, MainActivity.class);
+        startActivity(i);
     }
 
 }
